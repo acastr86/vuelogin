0fe9845401f32eb4c00394ee3ac2ac2e99b934a1
Adonis <acastr86@stu.southtexascollege.edu>
2021-11-10 22:57:12 -0600

    Added Basic Validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,14 +1,25 @@
 <script setup>
-import {ref} from "vue";
 import {useRouter} from "vue-router";
+import {useForm, useField} from "vee-validate";
+import * as yup from "yup";
+
+const schema = yup.object({
+    username: yup.string().required().email().label("Email"),
+    password: yup.string().required().min(8).label("Password"),
+});
+
+useForm({
+    validationSchema: schema,
+});
+
+const {value: username, errorMessage: emailError} = useField("username");
+const {value: password, errorMessage: passwordError} = useField("password");
+
 import UndrawLogin from "vue-undraw/UndrawLogin.vue";
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 
 const {isAuthenticated, login, signup, googleLogin} = useAuth();
-
-const username = ref("");
-const password = ref("");
 
 const router = useRouter();
 
@@ -50,10 +61,12 @@ const {ready, start} = useTimeout(3000, { controls: true });
         
         <UndrawLogin class="h-64 bg-white" />
         <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-            <input type="text" class="p-2 border-2 rounded-lg"
-              placeholder="Username" v-model="username" />
-              <input type="password" class="p-2 border-2 rounded-lg"
+            <input name="username" type="text" class="p-2 border-2 rounded-lg"
+              placeholder="Email" v-model="username" />
+              <span class="text-xs text-center text-red-500">{{emailError}}</span>
+              <input name="password" type="password" class="p-2 border-2 rounded-lg"
               placeholder="Password" v-model="password" />
+              <span class="text-xs text-center text-red-500">{{passwordError}}</span>
               <div class="flex space-x-2">
                   <button @submit.prevent="logginIn" type="submit" class="w-1/2 py-2 text-blue-200 bg-blue-600 rounded-lg">Login</button>
                   <button @click="signingUp" class="w-1/2 py-2 text-yellow-200 bg-yellow-600 rounded-lg">Sign Up</button>
